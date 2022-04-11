012791a4fa73a2d366022c797a0128652d4e59b2
Pete Pratt <peter.pratt@unipart.io>
2022-04-11 14:00:16 +0100

    [ADD] stock: Add method to get_next_picking_name
    Add a method to get the next picking name.
    
    User-story: 1215
    
    Signed-off-by: Pete Pratt <peter.pratt@unipart.io>

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -638,7 +638,7 @@ class Picking(models.Model):
                 }}
 
 
-    def get_next_picking_name(self, picking_type=None, **kwargs):
+    def get_next_picking_name(self, vals, picking_type, **kwargs):
         """Return the next picking name, core odoo is from the picking type sequence"""
         return picking_type.sequence_id.next_by_id()
 
@@ -648,7 +648,7 @@ class Picking(models.Model):
         picking_type = self.env['stock.picking.type'].browse(vals.get('picking_type_id', defaults.get('picking_type_id')))
         if vals.get('name', '/') == '/' and defaults.get('name', '/') == '/' and vals.get('picking_type_id', defaults.get('picking_type_id')):
             if picking_type.sequence_id:
-                vals['name'] = self.get_next_picking_name(picking_type)
+                vals['name'] = self.get_next_picking_name(vals, picking_type)
 
         # As the on_change in one2many list is WIP, we will overwrite the locations on the stock moves here
         # As it is a create the format will be a list of (0, 0, dict)
