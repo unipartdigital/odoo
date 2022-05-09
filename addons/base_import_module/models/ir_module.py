4e5d9327960b5bc808558085d8ce8d2286229e1c
Jack <jack.birch@unipart.io>
2022-05-09 09:26:46 +0100

    [IMP] base_import_module: Reword user error message.
    Story/1670
    
    Signed off by: Jack Birch <jack.birch@unipart.io>

--- addons/base_import_module/models/ir_module.py
+++ addons/base_import_module/models/ir_module.py
@@ -56,7 +56,7 @@ class IrModule(models.Model):
                 )
             raise UserError(err)
         elif 'web_studio' not in installed_mods and _is_studio_custom(path):
-            raise UserError(_("Studio customizations require the Odoo Studio app."))
+            raise UserError(_("Studio customizations require the Studio app."))
 
         mod = known_mods_names.get(module)
         if mod:
