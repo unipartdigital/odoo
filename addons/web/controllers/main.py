5fbbcbbde7179d806bfd796f648a02e44b9daf03
Kevin Dwyer <kevin.dwyer@unipart.com>
2025-10-31 16:04:58 +0000

    [FIX] web: Jinja2 v3 compatibility
    SE-2224
    
    Signed-off-by: Kevin Dwyer <kevin.dwyer@unipart.com>
    
    Always use FileSystemLoader
    
    cf https://stackoverflow.com/q/67676096/5320906

--- addons/web/controllers/main.py
+++ addons/web/controllers/main.py
@@ -52,12 +52,8 @@ from odoo.service import db
 
 _logger = logging.getLogger(__name__)
 
-if hasattr(sys, 'frozen'):
-    # When running on compiled windows binary, we don't have access to package loader.
-    path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'views'))
-    loader = jinja2.FileSystemLoader(path)
-else:
-    loader = jinja2.PackageLoader('odoo.addons.web', "views")
+path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'views'))
+loader = jinja2.FileSystemLoader(path)
 
 env = jinja2.Environment(loader=loader, autoescape=True)
 env.filters["json"] = json.dumps
